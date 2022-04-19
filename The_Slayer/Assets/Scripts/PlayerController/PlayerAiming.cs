bfbb83cb524bc4dcd955ba53721ec6fa2198dcdb
Daniel Reyes <daniereyes@csumb.edu>
2022-04-19 16:42:49 -0700

    Added mouse smoothing option

--- The_Slayer/Assets/Scripts/PlayerController/PlayerAiming.cs
+++ The_Slayer/Assets/Scripts/PlayerController/PlayerAiming.cs
@@ -8,7 +8,14 @@ public class PlayerAiming : MonoBehaviour
     public Transform playerCamera;
     public float mouseSensitivity = 2.8f;
     private float cameraPitch = 0.0f;
+
+    public bool mouseSmoothing = false;
+    [Range(0.0f, 0.5f)] public float mouseSmoothTime = 0.03f;
+    
     public bool lockCursor = true;
+    
+    private Vector2 currentMouseDelta = Vector2.zero;
+    private Vector2 currentMouseDeltaVelocity = Vector2.zero;
     
     // Start is called before the first frame update
     void Start()
@@ -28,11 +35,23 @@ public class PlayerAiming : MonoBehaviour
 
     void UpdateMouseLook()
     {
-        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        cameraPitch -= mouseDelta.y * mouseSensitivity;
-        cameraPitch = Mathf.Clamp(cameraPitch, -88.5f, 88.5f);
-        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
-        transform.Rotate(Vector3.up * mouseDelta.x * mouseSensitivity);
+        if (mouseSmoothing)
+        {
+            Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
+            cameraPitch -= currentMouseDelta.y * mouseSensitivity;
+            cameraPitch = Mathf.Clamp(cameraPitch, -88.5f, 88.5f);
+            playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+            transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
+        }
+        else
+        {
+            Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            cameraPitch -= mouseDelta.y * mouseSensitivity;
+            cameraPitch = Mathf.Clamp(cameraPitch, -88.5f, 88.5f);
+            playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+            transform.Rotate(Vector3.up * mouseDelta.x * mouseSensitivity);
+        }
         
     }
 }
